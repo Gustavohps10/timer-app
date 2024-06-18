dc6d845d649a65a23d3f95364859d2d6c8c2399b
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-18 20:59:33 -0300

    chore: router config

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
+import { Router } from './components/Router'
+import { BrowserRouter } from 'react-router-dom'
+
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      test
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
