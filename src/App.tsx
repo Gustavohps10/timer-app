fedbf70db5afe7d515f4c547f6cef31a6d0180f4
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-17 23:23:45 -0300

    chore: add styled components global config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import styled, { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/gobal"
+
+const StyledButton = styled.button`
+  background-color: ${({theme}) => theme.colors.primary};
+  color: #fff;
+  outline: none;
+  border: none;
+  padding: 0.4rem 1rem;
+  border-radius: .2rem;
+  cursor: pointer;
+  transition: all .2s ease;
+
+  &:hover{
+   background-color: #000;
+  }
+`
+
 function App() {
   return (
-   <>Start</>
+   <ThemeProvider theme={defaultTheme}>
+    <StyledButton>Test</StyledButton>
+    <GlobalStyle/>
+   </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+    colors: {
+        primary: "#8257e6"
+    }
+}
