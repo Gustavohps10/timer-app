f74353abbf068a2dd80a6edc461b4e1da107c28d
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-22 04:06:30 -0300

    feat: handle new cycle form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,11 +8,37 @@ import {
   TaskInput,
 } from './styles'
 import { Button } from '../../components/Button'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const newCycleFormValidationSchema = z.object({
+  task: z.string().min(1),
+  minutesAmount: z.number().min(5).max(60),
+})
+
+type NewCycleFormData = z.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="#">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -20,6 +46,7 @@ export function Home() {
             id="task"
             placeholder="de um nome para seu projeto"
             list="task-suggestions"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -36,6 +63,7 @@ export function Home() {
             id="minutesAmount"
             placeholder="00"
             step={5}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos</span>
@@ -49,7 +77,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <Button type="submit">
+        <Button type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Começar
         </Button>
